396e37e72c1883e5a913be0f9386711022eddfe1
Kayden Kara <kayden@bbd.co.za>
2023-06-24 17:27:27 +0200

    Added actual calls to the database for logins and registering

--- auth/index.js
+++ auth/index.js
@@ -1,79 +1,63 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const bodyParser = require('body-parser');
+const { json } = require('body-parser');
 const { dbConnection } = require('./api');
 const bouncer = require ('express-bouncer')(900000, 900000, 3);
 const getSecrets = require('./getSecrets') 
 const hash = require('./hash/hash')
+const db = require('./db/db')
 
 const app = express();
-app.use(bodyParser.json());
+app.use(json());
 
 let jwtToken;
 
-getSecrets.getSecret("prod/redgreenlight/jwt")
-.then((secretValue) => {
-  jwtToken = secretValue;
-})
-.catch((error) => {
-  console.error("Error retrieving secret:", error);
-});
-
-bouncer.blocked = function (req, res, next, remaining)
-{
-    res.status(429, "Too many requests have been made, " +
-        "please wait " + remaining / 1000 + " seconds");
+// Function to handle bouncer blocked requests
+bouncer.blocked = function (req, res, _, remaining) {
+    res.status(429).send("Too many requests have been made, please wait " + remaining / 1000 + " seconds");
 };
 
-// Mock database for demo purposes
-const users = [
-  { id: 1, username: 'user1', password: 'password1' },
-  { id: 2, username: 'user2', password: 'password2' },
-];
+async function initializeJwtToken() {
+  try {
+    jwtToken = await getSecrets.getSecret("prod/redgreenlight/jwt");
+  } catch(error) {
+    console.error("Error retrieving secret:", error);
+  }
+}
 
+initializeJwtToken();
 
-// Authentication endpoint
-app.post('/login', bouncer.block, (req, res) => {
+app.post('/login', bouncer.block, async (req, res) => {
   const { username, password } = req.body;
-  
-  // Mock user authentication
-  const user = users.find((u) => u.username === username && u.password === password);
 
-  //Get encrypyed password from db
+  try {
+    if (!await db.doesUserExist(username) || !await hash.verifyPassword(password, await db.getUserPassword(username))) {
+      return res.status(401).json({ error: 'Invalid User or Credentials' });
+    }
 
-  //Compare password from request with encrypted password from db
-  // can use this code
-  // const doesMatch = await hash.verifyPassword(password,encryptedPassword);
-  
-  if (!user) {
-    return res.status(401).json({ error: 'Invalid credentials' });
-  }
-  else {
     bouncer.reset(req);
+    const token = jwt.sign({ userName: username }, jwtToken, { expiresIn: '1h' });
+    res.json({ token });
+  } catch(error) {
+    res.status(500).json({ error: 'An error occurred during login' });
   }
-  
-  // Generate JWT token with a secret key
-  const token = jwt.sign({ userId: user.id }, jwtToken, { expiresIn: '1h' });
-  
-  // Return the JWT token to the client
-  res.json({ token });
 });
 
 app.post('/register', bouncer.block, async (req, res) => {
   const { username, password } = req.body;
-  
-  //first check if user exists in db
 
-  //if it does encrypt password to be stored
-  const encryptedPassword = await hash.encrypt(password);
-
-  //store in db afterwards
-
+  try {
+    if (await db.doesUserExist(username)){
+      return res.status(409).json({ error: 'User already exists' });
+    }
+    await db.registerUser(username, await hash.encrypt(password));
+    res.status(200).json({message: 'User created successfully'});
+  } catch(error) {
+    res.status(500).json({ error: 'An error occurred during registration' });
+  }
 });
 
-
-// Start the server
-const port = 4000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Authentication server is running on port ${port}`);
 });
